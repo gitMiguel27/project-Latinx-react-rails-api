5126570698851599defd4ce921b914409834a1cd
Miguel Nazario <mnazario35@gmail.com>
2022-04-18 11:25:10 -0400

    almost done seeding

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
   resources :countries
   resources :artworks
   resources :users
   resources :artists
+
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
