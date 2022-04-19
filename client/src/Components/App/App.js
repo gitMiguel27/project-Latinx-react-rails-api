01d9997575fc535818b0b68b4fb5804adeb0f1f7
Miguel Nazario <mnazario35@gmail.com>
2022-04-19 10:00:58 -0400

    fixed router stuff

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,6 +1,6 @@
-import {React, Fragment} from 'react';
+import {React} from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../Home'
 import Navbar from '../Navbar'
 import ArtistPage from '../ArtistPage/ArtistPage';
@@ -9,17 +9,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Fragment>
+        <Switch>
         <Navbar />
-          <Routes >
             <Route exact path="/">
               <Home />
             </Route>
             <Route path="/artist">
               <ArtistPage />
             </Route>
-          </Routes>
-        </Fragment>
+        </Switch>
       </BrowserRouter>
     </div>
   );
